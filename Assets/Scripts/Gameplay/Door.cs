d51f038ad9bd516af67aa782e2921670dbde5f4f
Baurzhan Sabyr <bsabyr1878@gmail.com>
2025-05-04 13:44:17 +0200

    Door

---
+++ Assets/Scripts/Gameplay/Door.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class Door: MonoBehaviour
+    {
+        [SerializeField] private Transform pivot;
+        [SerializeField] private float openSpeed = 20f;
+        
+        private bool isInteracting;
+        private bool isInZone;
+        private float currentRotation;
+        private const float MaxOpenAngle = 90f;
+
+        private void Update()
+        {
+            isInteracting = isInZone && Input.GetKey(KeyCode.E);
+            
+            if (isInteracting)
+            {
+                RotateDoor();
+            }
+        }
+        
+        private void RotateDoor()
+        {
+           currentRotation += openSpeed * Time.deltaTime;
+           
+           currentRotation = Mathf.Clamp(currentRotation, 0, MaxOpenAngle);
+           
+           Quaternion targetRotation = Quaternion.Euler(0, currentRotation, 0);
+           
+           pivot.rotation = targetRotation;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isInZone = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                isInZone = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,64 +1,70 @@
 ﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private CharacterController controller;
-    [SerializeField] private Transform playerCamera;
-
-    [SerializeField] private float speed = 3f;
-    [SerializeField] private float gravity = -30f;
-    [SerializeField] private float sensitivity = 400f;
-
-    private float xRotation;
-    private Vector3 velocity;
-
-    [HideInInspector]
-    public bool verticalMovement = false;
-    // public float gravity = -30f;
-    
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+        [SerializeField] private CharacterController controller;
+        [SerializeField] private Transform playerCamera;
 
-    void Update()
-    {
-        Vector3 move = Vector3.zero;
+        [SerializeField] private float normalSpeed = 3f;
+        [SerializeField] private float walkSpeed = 1f;
+        [SerializeField] private float gravity = -30f;
+        [SerializeField] private float sensitivity = 400f;
 
-        // Player Movement
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        // Camera Movement
-        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        private float speed;
+        private float xRotation;
+        private Vector3 velocity;
 
-        if (verticalMovement)
+        [HideInInspector]
+        public bool verticalMovement = false;
+        
+        private void Start()
         {
-            move = transform.forward * z;
+            Cursor.lockState = CursorLockMode.Locked;
         }
-        else
+
+        private void Update()
         {
-            move = transform.right * x + transform.forward * z;
+            Vector3 move = Vector3.zero;
+
+            // Player Movement
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            // Camera Movement
+            float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
             
-            xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            if (verticalMovement)
+            {
+                move = transform.forward * z;
+            }
+            else
+            {
+                move = transform.right * x + transform.forward * z;
+                
+                xRotation -= mouseY;
+                xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-            playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+                playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-            transform.Rotate(Vector3.up * mouseX);
-        }
-        
-        if (controller.isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        else
-        {
-            velocity.y += gravity * Time.deltaTime;
-        }
+                transform.Rotate(Vector3.up * mouseX);
+            }
+            
+            if (controller.isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+            else
+            {
+                velocity.y += gravity * Time.deltaTime;
+            }
 
-        move = (move * speed) + velocity;
-        
-        controller.Move(move * Time.deltaTime);
+            speed = Input.GetKey(KeyCode.LeftShift) ? walkSpeed : normalSpeed;
+            
+            move = (move * speed) + velocity;
+            
+            controller.Move(move * Time.deltaTime);
+        }
     }
 }
